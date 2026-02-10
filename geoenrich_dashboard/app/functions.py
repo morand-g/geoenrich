fcbf97edc631f04a1466312b652c6253c1493369
morand-g <gaetan.morand@outlook.com>
2026-02-10 16:02:20 +0100

    modified:   ../geoenrich/enrichment.py 	modified:   Dockerfile 	modified:   app/functions.py

--- geoenrich_dashboard/app/functions.py
+++ geoenrich_dashboard/app/functions.py
@@ -2,7 +2,6 @@ from geoenrich.enrichment import *
 from pathlib import Path
 from tqdm.auto import tqdm
 import time
-from celery import Celery
 from flask_socketio import SocketIO
 
 # Celery worker needs its own SocketIO client
@@ -10,10 +9,6 @@ socketio = SocketIO(
     message_queue="redis://localhost:6379/0",
     async_mode="threading",
 )
-
-def update_bar(enrichment_id, **kwargs):
-    socketio.emit('enrichment_status', {'enrichment_id': enrichment_id, **kwargs})
-                  
 
 def get_progress_callback(enrichment_id):
 
@@ -25,12 +20,15 @@ def get_progress_callback(enrichment_id):
         def update(self, n=1):
             super().update(n)
 
+            if not self.total:
+                return
+
             # throttle: 5–10 Hz
             now = time.time()
             if now - self._last_emit < 0.2:
                 return
 
             self._last_emit = now
-            socketio.start_background_task(target=update_bar, enrichment_id= enrichment_id, status= "PROGRESS", progress= self.n / self.total * 100 )
+            socketio.emit('enrichment_status', {'enrichment_id': enrichment_id, 'status': "PROGRESS", 'progress': self.n / self.total * 100 })
 
-
+    return TqdmWithCallback
