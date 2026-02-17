9aabaa660a87c4a302bf6937d620bee6130ed532
morand-g <gaetan.morand@outlook.com>
2026-02-17 16:14:25 +0100

    modified:   ../.gitignore 	modified:   ../geoenrich_online/Dockerfile 	modified:   ../geoenrich_online/app/copy_my_files.py

--- geoenrich_online/app/copy_my_files.py
+++ geoenrich_online/app/copy_my_files.py
@@ -1,12 +1,9 @@
 from pathlib import Path
 import shutil
 
-if Path('/home/data/personal_catalog.csv').exists():
-    shutil.copy(Path('/home/data/personal_catalog.csv'), Path('/usr/local/lib/python3.10/site-packages/geoenrich/data/personal_catalog.csv'))
+if Path('/app/data/.netrc').exists():
+    shutil.copy(Path('/app/data/.netrc'), Path('/root/.netrc'))
 
-if Path('/home/data/.netrc').exists():
-    shutil.copy(Path('/home/data/.netrc'), Path('/root/.netrc'))
-
-if Path('/home/data/.copernicusmarine-credentials').exists():
+if Path('/app/data/.copernicusmarine-credentials').exists():
     Path('/root/.copernicusmarine').mkdir(exist_ok=True)
-    shutil.copy(Path('/home/data/.copernicusmarine-credentials'), Path('/root/.copernicusmarine/.copernicusmarine-credentials'))
+    shutil.copy(Path('/app/data/.copernicusmarine-credentials'), Path('/root/.copernicusmarine/.copernicusmarine-credentials'))
